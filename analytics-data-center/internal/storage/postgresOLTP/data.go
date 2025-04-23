84c42d56d12586ce4facffce7b6ed7bf3072cae2
Ардышев Александр Алексеевич <a.ardyshev@ctrl2go.solutions>
2025-04-23 10:30:16 +0300

    adding repo func SelectDataToInsert

--- analytics-data-center/internal/storage/postgresOLTP/data.go
+++ analytics-data-center/internal/storage/postgresOLTP/data.go
@@ -23,6 +23,43 @@ func (p *PostgresOLTP) GetCountInsertData(ctx context.Context, query string) (in
 
 }
 
-func (p *PostgresOLTP) InsertDataToTempTable(ctx context.Context, query string) (bool, error) {
-	panic("")
+func (p *PostgresOLTP) SelectDataToInsert(ctx context.Context, query string) ([]map[string]interface{}, error) {
+	const op = "Storage.PostgresOLTP.SelectDataToInsert"
+	log := p.Log.With(
+		slog.String("op", op),
+		slog.String("query", query),
+	)
+	log.Info("выборка данных для вставки")
+
+	rows, err := p.Db.QueryContext(ctx, query)
+	if err != nil {
+		log.Error("ошибка получения данных для вставки", slog.String("ошибка", err.Error()))
+		return nil, err
+	}
+	defer rows.Close()
+	columns, err := rows.Columns()
+	results := make([]map[string]interface{}, 0)
+	if err != nil {
+		log.Error("ошибка получения колонок", slog.String("ошибка", err.Error()))
+		return nil, err
+	}
+	for rows.Next() {
+		values := make([]interface{}, len(columns))
+		valuesPointers := make([]interface{}, len(columns))
+
+		for i := range values {
+			valuesPointers[i] = &values[i]
+		}
+		if err := rows.Scan(valuesPointers...); err != nil {
+			log.Error("ошибка сканирования строк", slog.String("ошибка", err.Error()))
+		}
+		rowMap := make(map[string]interface{})
+		for i, col := range columns {
+			rowMap[col] = values[i]
+		}
+
+		results = append(results, rowMap)
+
+	}
+	return results, nil
 }
